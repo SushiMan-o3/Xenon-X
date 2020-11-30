a7d1a1072c603947ce68bec4c4f122a14b11eba4
SushiMan <45275055+SushiMan-o3@users.noreply.github.com>
2020-11-30 10:56:14 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import praw
+
+token = ''
+
+reddit = reddit = praw.Reddit(client_id = 'bot id',
+        client_secret = 'client sercert',
+        username = 'Account username',
+        password = 'Account password',
+        user_agent = 'Reddit bot name by account username')
