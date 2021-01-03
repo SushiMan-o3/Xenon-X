de59d2126da56a47d43634bfd49cffee6e952beb
SushiMan <45275055+SushiMan-o3@users.noreply.github.com>
2021-01-03 15:33:23 -0500

    Update currency.py

--- cogs/currency.py
+++ cogs/currency.py
@@ -5,7 +5,12 @@ import random
 
 def colour():
     colours = [0x921cff, 0x00ff7f, 0xff9b38, 0xff0000, 0x0900ff]
-    return random.choice(colours)  
+    return random.choice(colours) 
+
+def open_currency():
+    with open('currency/currency.json') as f:
+        balance = json.load(f)
+        return balance
 
 class currency(commands.Cog, name='Currency'):
     def __init__(self, bot):
@@ -17,7 +22,7 @@ class currency(commands.Cog, name='Currency'):
 
     @commands.command(description = "Registers you into our currency system.")
     async def register(self, ctx):
-        with open('currency.json') as f:
+        with open('currency/currency.json') as f:
             balance = json.load(f)
         
         member = str(ctx.author.id)
@@ -27,13 +32,13 @@ class currency(commands.Cog, name='Currency'):
         else:
             balance[str(member)] = int(500)
 
-            with open('currency.json', 'w') as f:
+            with open('currency/currency.json', 'w') as f:
                 json.dump(balance, f, indent = 4)
-                await ctx.send(f"{ctx.author.mention} is now registered!")
+            await ctx.send(f"{ctx.author.mention} is now registered!")
 
     @commands.command(description="Checks the user's balance!")
     async def balance(self, ctx, *, member: discord.Member = None):
-        with open('currency.json') as f:
+        with open('currency/currency.json') as f:
                     balance = json.load(f)
         if member == None:
             if str(ctx.author.id) not in balance.keys():
@@ -51,19 +56,19 @@ class currency(commands.Cog, name='Currency'):
     @commands.command(description = "Work's for money")
     @commands.cooldown(1, 60**3, commands.BucketType.user)
     async def work(self, ctx):
-        with open('currency.json') as f:
+        with open('currency/currency.json') as f:
                     balance = json.load(f)
         if str(ctx.author.id) not in balance.keys():
             await ctx.send("You haven't registered yet. Use `!register` to register for the currency bot!")
         else:
-            with open('currency.json') as f:
+            with open('currency/currency.json') as f:
                 balance = json.load(f)
 
             add = 500 + balance[str(ctx.author.id)]
 
             balance[str(ctx.author.id)] = add
 
-            with open('currency.json', 'w') as f:
+            with open('currency/currency.json', 'w') as f:
                 json.dump(balance, f, indent = 4)
 
             embed = discord.Embed(description = f"{ctx.author.mention} worked and gained $500, and now has ${balance[str(ctx.author.id)]}", color = colour())
@@ -75,17 +80,17 @@ class currency(commands.Cog, name='Currency'):
         try:
             if ctx.author.id in user_can:
                 if member == None:
-                    with open('currency.json') as f:
+                    with open('currency/currency.json') as f:
                         balance = json.load(f)
                     balance[str(ctx.author.id)] = value
-                    with open('currency.json', 'w') as f:
+                    with open('currency/currency.json', 'w') as f:
                         json.dump(balance, f, indent = 4)
                     await ctx.send(f'{ctx.author.mention} has ${balance[str(ctx.author.id)]}')
                 else:
-                    with open('currency.json') as f:
+                    with open('currency/currency.json') as f:
                         balance = json.load(f)
                     balance[str(member.id)] = value
-                    with open('currency.json', 'w') as f:
+                    with open('currency/currency.json', 'w') as f:
                         json.dump(balance, f, indent = 4)
 
                     await ctx.send(f'{member.mention} has ${balance[str(member.id)]}')
@@ -96,7 +101,7 @@ class currency(commands.Cog, name='Currency'):
 
     @commands.command(description = 'Leader board for who has the most money!')
     async def leaderboard(self, ctx):
-        with open('currency.json') as f:
+        with open('currency/currency.json') as f:
             balance = json.load(f)
         top_5 = sorted(balance, key=balance.get, reverse=True)[:5]
         five_top = []
@@ -110,9 +115,9 @@ class currency(commands.Cog, name='Currency'):
 
     @commands.command()
     async def buy(self, ctx, *, item: str):
-        with open('currency.json') as f:
+        with open('currency/currency.json') as f:
             balance = json.load(f)
-        with open('shop.json') as f:
+        with open('currency/shop.json') as f:
             shop = json.load(f)
         item = item.lower()
         if item in shop.keys():
@@ -123,17 +128,17 @@ class currency(commands.Cog, name='Currency'):
                     await ctx.send(f"You don't have enough money to buy {item.title()}!")
                 else:
                     balance[str(ctx.author.id)] = user - item_price
-                    with open("currency.json", 'w') as f:
+                    with open("currency/currency.json", 'w') as f:
                         json.dump(balance, f, indent = 4)
                     await ctx.send(f"You bought the {item.title()}, and it added to your inventory!")
                     #add it into another json file where key = id and a list with all items as a value
-                    with open("items.json") as f:
+                    with open("currency/items.json") as f:
                         items = json.load(f)
                     if str(ctx.author.id) in items.keys():
                         items[str(ctx.author.id)].append(item)
                     else:
                         items[str(ctx.author.id)] = [item]
-                    with open("items.json", 'w') as f:
+                    with open("currency/items.json", 'w') as f:
                         json.dump(items, f, indent = 4)
             else:
                 await ctx.send("Do `!register` to sign up!")
@@ -142,12 +147,12 @@ class currency(commands.Cog, name='Currency'):
 
     @commands.command()
     async def inventory(self, ctx):
-        with open('items.json') as f:
+        with open('currency/items.json') as f:
             items = json.load(f)
         if str(ctx.author.id) not in items.keys():
             await ctx.send("You don't have anything in your inventory. Buy some stuff using !buy <item>")
         else:
-            with open('shop.json') as f:
+            with open('currency/shop.json') as f:
                 shop = json.load(f)
             itemsss = []
             for x in shop:
@@ -158,6 +163,15 @@ class currency(commands.Cog, name='Currency'):
             embed = discord.Embed(title = 'Items', description = f'*You have...*\n\n {x}', color = colour())
             embed.set_footer(text = f"{ctx.author}'s items")
             await ctx.send(embed=embed)
+
+    @commands.command()
+    async def clear(self, ctx):
+        balance = {}
+        clearing = ['currency/items.json', 'currency/currency.json']
+        for i in clearing:
+            with open(i, 'w') as f:
+                json.dump(balance, f, indent = 4)
+        await ctx.send("All data is cleared.")
         
 
 def setup(bot):
